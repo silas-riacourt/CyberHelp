4ff64f64f59faf9d13189a73d946d842067e1496
Juvamine B <justine.boissard@gmail.com>
2023-03-22 10:55:27 +0000

    création page "a propos"

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,50 @@
 <template>
-    test
+    <div class="main_title">
+        <h1> A propos de nous </h1>
+    </div>
+    <div class="topo">
+        <div class="title">
+            <h2> Notre entreprise </h2>
+        </div>
+        <div class="text">
+            <p>
+                S’appuyant sur plus de 20 ingénieurs et spécialistes hautement qualifiés, ACG Cybersecurity a développé des compétences dans l’évaluation et l’audit de la sécurité des systèmes d’information, l’analyse des risques IT, le développement des politiques de sécurité de l’information et des Plans de continuité d’activité, et la mise en place des SMSI ISO 27001. A l’écoute de ses clients, ACG Cybersecurity est présente dans les secteurs clés de l’économie. ACG Cybersecurity est un cabinet indépendant de tout éditeur ou constructeur. Nous conseillons nos clients et les accompagnons dans la définition de leurs besoins et des réponses cyber à apporter sur le seul critère de leurs intérêts
+            </p>
+        </div>
+        <div class="title">
+            <h2> Nos missions </h2>
+        </div>
+        <div class="text">
+            <p>
+                Aider nos clients, entreprises et opérateurs, à intégrer les nouvelles technologies de l’information et de la communication, en leur offrant les solutions et les services qui rendent leurs réseaux et systèmes de communication disponibles, performants, sécurisés et évolutifs. Cette position s’appuie sur une large expérience dans la conception et la mise en œuvre de systèmes et de réseaux complexes ou critiques, acquise grâce:
+            </p>
+            <ul>
+                <li>aux fortes compétences de ACG Cybersecurity en ingénierie des Réseaux, Sécurité.</li>
+                <li>à une offre de services complète et intégrée, couvrant l’audit, le conseil, le déploiement, le support, la maintenance et l’exploitation de la solution.</li>
+                <li>à un investissement permanent dans les nouveaux domaines technologiques</li>
+                <li>à des partenariats très étroits avec les principaux leaders du marché, constructeurs d’équipement de réseaux et éditeurs de logiciels</li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+
+<style>
+    .main_title {
+        color: white;
+        margin: 15px 0;
+        text-transform: uppercase;
+    }
+    .topo {
+        color: white;
+        background-color: rgba(16, 15, 15, 0.6);
+    }
+    .text {
+        padding: 20px 10px;
+        text-align: left;
+    }
+    .text ul {
+        margin-left: 50px;
+        margin-top: 10px;
+    }
+</style>
