f5dcb3fdd8f5444b96cf8c133cb651614872fb02
Silass_ <silasdu22@gmail.com>
2023-03-22 09:05:03 +0100

    feat : components

---
+++ src/components/QuestionButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn size="x-large" color="white" @click="$emit('click')">{{text}}</v-btn>
+</template>
+
+<script >
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <v-card  variant="outlined" color="white" height="400px">
-  <h1>ICI LA QUESTION</h1>
-        <v-btn size="x-large" >CHOIX A</v-btn>
+  <v-card color="white" height="max-content" class="main--card" min-height="fit-content">
+    <v-card-title class="text-uppercase text-white">
+      <h1>{{question}}</h1>
+    </v-card-title>
+
+  <QuestionButton text="Je suis un particulier" @click="console.log('click')"></QuestionButton>
+    <QuestionButton text="Je suis un particulier" @click="console.log('click')"></QuestionButton>
   </v-card>
 </template>
 
 <script setup>
+import QuestionButton from '@/components/QuestionButton.vue';
+ 
  
 </script>
+<script>
+export default {
+  data() {
+    return {
+      question: 'êtes-vous un particulier ou un professionnel ?'
+    }
+  }
+}
+</script>
+
+<style>
+.main--card {
+ 
+  background-color: rgba(16, 15, 15, 0.60)!important;
+}
+</style>
